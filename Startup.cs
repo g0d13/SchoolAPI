8ba90464f01683c41a27552006d2627b0ba756d3
Juan Diego Parra Mendez <g0d13@outlook.com>
2021-05-25 00:04:03 -0500

    use swagger in production

--- Startup.cs
+++ Startup.cs
@@ -43,8 +43,8 @@ namespace SchoolAPI
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchoolAPI v1"));
             }
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchoolAPI v1"));
 
             app.UseHttpsRedirection();
 
