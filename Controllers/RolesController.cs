1f17e554a09eb7ec99767e591226f062fb15f3f7
Juan Diego Parra Mendez <g0d13@outlook.com>
2021-05-25 14:04:25 -0500

    agregar controlador de roles

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolAPI;
+using SchoolAPI.Models;
+
+namespace SchoolAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public RolesController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Roles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rol>>> GetRoles()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        // GET: api/Roles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rol>> GetRol(int id)
+        {
+            var rol = await _context.Roles.FindAsync(id);
+
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return rol;
+        }
+
+        // PUT: api/Roles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRol(int id, Rol rol)
+        {
+            if (id != rol.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rol).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RolExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Roles
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Rol>> PostRol(Rol rol)
+        {
+            _context.Roles.Add(rol);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRol", new { id = rol.Id }, rol);
+        }
+
+        // DELETE: api/Roles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRol(int id)
+        {
+            var rol = await _context.Roles.FindAsync(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            _context.Roles.Remove(rol);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RolExists(int id)
+        {
+            return _context.Roles.Any(e => e.Id == id);
+        }
+    }
+}
