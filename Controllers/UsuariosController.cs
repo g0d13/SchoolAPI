e65e14e31fa89f98df1ee0eb6590d51af04ca102
Juan Diego Parra Mendez <g0d13@outlook.com>
2021-05-24 23:30:09 -0500

    agregar controlador de usuarios

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolAPI;
+using SchoolAPI.Models;
+
+namespace SchoolAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public UsuariosController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+        
+        private string Hash(string texto)
+        {
+            using(SHA1Managed sha1 = new SHA1Managed())
+            {
+                byte[] stringTemp = System.Text.Encoding.UTF8.GetBytes(texto);
+                var hash = sha1.ComputeHash(stringTemp);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        // PUT: api/Usuarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut]
+        public async Task<IActionResult> PutUsuario(Usuario usuario)
+        {
+            usuario.Pass = Hash(usuario.Pass);
+            _context.Entry(usuario).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // POST: api/Usuarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario([FromBody]Usuario usuario)
+        {
+            usuario.Pass = Hash(usuario.Pass);
+            await _context.Usuarios.AddAsync(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuario(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        
+    }
+}
